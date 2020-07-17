8e0d16210ab3b1d4c7cfb12027e5be23bcdbe8b5
sgt1796 <sgt1796@gmail.com>
2020-07-17 14:46:28 -0400

    Update FASTA_analysis.py
    add function to view multiple frames
    add legend to plot
    add label to long ORFs

--- FASTA_analysis.py
+++ FASTA_analysis.py
@@ -49,11 +49,12 @@ class FastaAna:
         #end = int(r.split(",")[1])
         if(end == -1):
             end = self.length - 1
-        for x in range(start,end):
-            if(seq[x] == 'A' and seq[x+1] == 'T' and seq[x+2] == 'G'):
-                pos.append(x)
+        while start < end-2:
+            if(seq[start] == 'A' and seq[start+1] == 'T' and seq[start+2] == 'G'):
+                pos.append(start)
                 count += 1
-                x += 2
+                start += 2
+            start += 1
         #print("total " + str(count)+ " START found:")
         #print(pos)
         return pos
@@ -70,25 +71,32 @@ class FastaAna:
         #end = int(r.split(",")[1])
         if(end == -1):
             end = self.length - 1
-        for x in range(start,end):
+        while start < end-2:
             stop = False
-            if(seq[x] == 'T'):
-                if(seq[x+1] == 'A'):
-                    if(seq[x+2] == 'G' or seq[x+2] == 'A'):
+            if(seq[start] == 'T'):
+                if(seq[start+1] == 'A'):
+                    if(seq[start+2] == 'G' or seq[start+2] == 'A'):
                         stop = True
-                if(seq[x+1] == 'G'):
-                    if(seq[x+2] == 'A'):
+                if(seq[start+1] == 'G'):
+                    if(seq[start+2] == 'A'):
                         stop = True
             if(stop):
-                pos.append(x)
+                pos.append(start)
                 count += 1
-                x += 2
+                start += 2
+            start += 1
         #print("total " + str(count)+ " STOP found:")
         #print(pos)
         return pos
 
-    def findORF(self, fr, rev = False):
-        #not fix: need to add frame
+    def findORF(self, fr = 1):
+        #There are 6 frames in total: 1, 2, 3, -1, -2, -3
+        rev = False
+        if fr < 0:
+            rev = True
+            fr = -fr
+
+        #find all start and stop condons and store them in pStart and pStop
         pStart = []
         pStop = []
         for x in self.findSTART(0,-1,rev):
@@ -99,6 +107,8 @@ class FastaAna:
                 pStop.append(x)
         #print(pStart)
         #print(pStop)
+
+        #find ORF by the shortest distance
         hold, count = 0, 0
         ORF = []
         for x in pStart:
@@ -110,27 +120,38 @@ class FastaAna:
                         count += 1
                         break
 
-        print("Total "+str(count)+" ORF found in reading frame " + str(fr) + ":")
+        if(rev):
+            print("Total " + str(count) + " ORF found in reading frame -" + str(fr) + ":")
+        else:
+            print("Total " + str(count) + " ORF found in reading frame " + str(fr) + ":")
         print(ORF)
         return ORF
 
-    def plotORF(self,hold, *ORF):
+    def plotORF(self, threshold, *ORF, coor = 2000):
+        print(ORF)
         if(not ORF):
             ORF = [self.findORF(1)]
         x1 = np.arange(self.length)
         y1 = x1
+        count = 1
         for orf in ORF:
             x,y = [],[]
             for w in orf:
-                if(w[1]-w[0] > hold):
+                if(w[1]-w[0] > threshold):
                     x.append(w[0])
                     y.append(w[1])
                     plt.plot([w[0],w[0]], [w[0],w[1]])
-            plt.plot(x,y,'.',x1,y1,'--')
+                    if w[1]-w[0] > coor:
+                        plt.text(w[0]+10, w[1]+10, ' [%d-%d]\n %d nt'%(w[0], w[1], w[1]-w[0]))
+
+            plt.plot(x,y,'.' ,label = 'reading frame %d'%count)
+            plt.plot(x1, y1, '--')
+            plt.legend(loc='lower right')
+            count += 1
         plt.show()
 
     def revSeq(self, seq):
-        #reverse seq and return in a 5' to 3' order
+        #reverse seq
         rev = ""
         for x in seq:
             if(x == 'A'):
@@ -153,10 +174,14 @@ class FastaAna:
 
 
 
-covFile = r"/Users/GuotaiShen/Desktop/Bioinformatic/2019-nCoV/sequence.fasta"
-file1 =  r"/Users/GuotaiShen/Desktop/Bioinformatic/pNMT1-GNAS/Pnmt1-GNAS.txt"
+#covFile = r"/Users/GuotaiShen/Desktop/Bioinformatic/2019-nCoV/sequence.fasta
+covFile = r'C:\Users\Guotai Shen\Desktop\Genenome seq\2019-nCoV.fasta'
+#file1 =  r"/Users/GuotaiShen/Desktop/Bioinformatic/pNMT1-GNAS/Pnmt1-GNAS.txt"
 CoV = FastaAna(covFile)
-orf1 = CoV.findORF(1,rev=True)
-#orf2 = CoV.findORF(2)
-#orf3 = CoV.findORF(3)
-#CoV.plotORF(300, orf1, orf2, orf3)
+orf1 = CoV.findORF(1)
+orf2 = CoV.findORF(2)
+orf3 = CoV.findORF(3)
+Rorf1 = CoV.findORF(-1)
+Rorf2 = CoV.findORF(-2)
+Rorf3 = CoV.findORF(-3)
+#CoV.plotORF(100, orf1, orf2, orf3, Rorf1, Rorf2, Rorf3)
